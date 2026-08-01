6d29c3fa9b556d6f79b2c5fc669e018e6387e0b6
niranjandhami <niranjandhami530@gmail.com>
2026-08-01 23:47:12 +0530

    Update frontend API URL

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,5 +1,4 @@
-export const BASE_URL = "";
-export const USERS_URL = "/api/users";
+export const BASE_URL = "https://bookverse-backend-d9n3bvblj550s73982si0.onrender.com";export const USERS_URL = "/api/users";
 export const CATEGORY_URL = "/api/category";
 export const PRODUCT_URL = "/api/products";
 export const UPLOAD_URL = "/api/upload";
